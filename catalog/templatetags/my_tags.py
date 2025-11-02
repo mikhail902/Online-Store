58566e8ff46b17a3b8c725ff0cbee24ad52cc046
миша <misatoropkov@gmail.com>
2025-11-03 01:34:22 +0300

    endd

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter()
+def media_filter(path):
+    if path:
+        return f"media/{path}"
+    else:
+        return f'#'
